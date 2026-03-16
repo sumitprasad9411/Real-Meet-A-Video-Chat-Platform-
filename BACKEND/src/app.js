1c9ef7b361ed85061163585fd149448144672128
Sumit-Prasad <147540281+sumitprasad9411@users.noreply.github.com>
2026-03-17 02:53:40 +0530

    Ready for Deployement

---
+++ BACKEND/src/app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import {createServer} from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+
+app.set("port",(process.env.PORT || 8000))   //Gc2RcbLNVGl4nBcp
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended: true}));
+
+app.use("/api/v1/users", userRoutes );
+
+const start = async ()=>{
+app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://sumitprasad2106_db_user:Gc2RcbLNVGl4nBcp@cluster0.obniadn.mongodb.net/");
+
+    console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"),()=> {
+        console.log("Listening on port 8000")
+    });
+}
+
+
+start();

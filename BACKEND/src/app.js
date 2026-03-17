6c72841de835beb698fd66357e50ab67961425a7
Sumit-Prasad <147540281+sumitprasad9411@users.noreply.github.com>
2026-03-17 12:43:48 +0530

    fixed cors issue

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -18,13 +18,17 @@ const io = connectToSocket(server);
 
 
 app.set("port",(process.env.PORT || 8000))   //Gc2RcbLNVGl4nBcp
-app.use(cors({
+const corsOptions = {
   origin: "https://real-meet-f.onrender.com",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true
-}));
+  credentials: true,
+};
 
-app.options("*", cors());
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
+
+
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
 
